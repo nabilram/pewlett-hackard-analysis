43fe4d342745013302278adbf495c6d24aefdabf
nabilram <89229716+nabilram@users.noreply.github.com>
2021-10-07 22:14:03 -0700

    7.3.2

--- sql_employees.py
+++ sql_employees.py
@@ -1,13 +1,16 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
+     dept_no VARCHAR(10) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
 
+-- DROP TABLE departments CASCADE
+-- SELECT * FROM departments
+
 CREATE TABLE employees (
-     emp_no INT NOT NULL,
+     emp_no VARCHAR(10) NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -16,9 +19,12 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
+-- DROP TABLE employees CASCADE
+-- SELECT * FROM employees
+
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+    emp_no VARCHAR(10) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -26,8 +32,11 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+-- DROP TABLE dept_manager
+-- SELECT * FROM dept_manager
+
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
+  emp_no VARCHAR(10) NOT NULL,
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
@@ -35,9 +44,12 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+-- DROP TABLE salaries
+-- SELECT * from salaries
+
 CREATE TABLE dept_emp (
-  dept_no VARCHAR(4) NOT NULL,
-  emp_no INT NOT NULL,
+  emp_no VARCHAR(10) NOT NULL,
+  dept_no VARCHAR(10) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -45,12 +57,16 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+-- DROP TABLE dept_emp
+-- SELECT * from dept_emp
+
 CREATE TABLE titles (
-  emp_no INT NOT NULL,
+  emp_no VARCHAR(10) NOT NULL,
   title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  PRIMARY KEY (emp_no)
+  to_date DATE NOT NULL
+  --PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments
+DROP TABLE titles
+SELECT * FROM titles
