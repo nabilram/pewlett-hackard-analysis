7b1a7187758666ad79a65dcbc497c678b9147525
nabilram <89229716+nabilram@users.noreply.github.com>
2021-10-08 01:18:18 -0700

    deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -26,3 +26,22 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY(ut.title)
 ORDER BY ut.count DESC;
+
+-- DELIVERY 2
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+  ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+  ON (ti.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+  AND (de.to_date = '9999-01-01')
+ORDER BY (e.emp_no);
